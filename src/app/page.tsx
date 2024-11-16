aefba5c316830223750c8bc063da4cbbb9924dd5
Heydar <h_h_z_n@yahoo.com>
2024-11-16 08:17:00 +0330

    build: :lipstick: create sections in landing page
    create sections in landing page
    
    create sections in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import Brand from "@/view/Brand";
+import Collection from "@/view/Collection";
+import Connect from "@/view/Connect";
+import Details from "@/view/Details";
+import Drops from "@/view/Drops";
+import Footer from "@/view/Footer";
+import Header from "@/view/Header";
+import Nav from "@/view/Nav";
+import Popular from "@/view/Popular";
+import Resource from "@/view/Resource";
+import Trend from "@/view/Trend";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <Details />
+      <Drops />
+      <Collection />
+      <Trend />
+      <Resource />
+      <Popular />
+      <Connect />
+      <Brand />
+      <Footer />
+    </div>
+  );
 }
